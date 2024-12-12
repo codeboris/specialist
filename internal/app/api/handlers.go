4a9ce88549db7420ce3c4ea5e9981d4ffbf6b378
Boris Taiurskii <codeboris.spb@gmail.com>
2024-12-12 21:04:21 +0300

    add handlers

---
+++ internal/app/api/handlers.go
@@ -0,0 +1,239 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/codeboris/specialist/internal/app/models"
+	"github.com/gorilla/mux"
+)
+
+type Message struct {
+	StatusCode int    `json:"status_code"`
+	Message    string `json:"message"`
+	IsError    bool   `json:"is_error"`
+}
+
+func initHandlers(writer http.ResponseWriter) {
+	writer.Header().Set("Content-Type", "application/json")
+}
+
+func (api *API) GetAllArticles(writer http.ResponseWriter, req *http.Request) {
+	initHandlers(writer)
+	api.logger.Info("Get All Articles GET /api/v1/articles")
+	articles, err := api.storage.Article().SelectAll()
+	if err != nil {
+		api.logger.Info("Error while Articles.SelectAll: ", err)
+		msg := Message{
+			StatusCode: 501,
+			Message:    "We have some troubles to accessing database. Try again later",
+			IsError:    true,
+		}
+		writer.WriteHeader(501)
+		json.NewEncoder(writer).Encode(msg)
+		return
+	}
+	writer.WriteHeader(200)
+	json.NewEncoder(writer).Encode(articles)
+
+}
+
+func (api *API) PostArticle(writer http.ResponseWriter, req *http.Request) {
+	initHandlers(writer)
+	api.logger.Info("Post Article POST /api/v1/articles")
+	var article models.Article
+	err := json.NewDecoder(req.Body).Decode(&article)
+	if err != nil {
+		api.logger.Info("Invalid json recieved from client")
+		msg := Message{
+			StatusCode: 400,
+			Message:    "Provided json is invalid",
+			IsError:    true,
+		}
+		writer.WriteHeader(400)
+		json.NewEncoder(writer).Encode(msg)
+		return
+	}
+	a, err := api.storage.Article().Create(&article)
+	if err != nil {
+		api.logger.Info("Troubles while creating new article: ", err)
+		msg := Message{
+			StatusCode: 501,
+			Message:    "We have some troubles to accessing database. Try again later",
+			IsError:    true,
+		}
+		writer.WriteHeader(501)
+		json.NewEncoder(writer).Encode(msg)
+		return
+	}
+	writer.WriteHeader(201)
+	json.NewEncoder(writer).Encode(a)
+}
+
+func (api *API) GetArticleByID(writer http.ResponseWriter, req *http.Request) {
+	initHandlers(writer)
+	api.logger.Info("Get Article by ID GET /api/v1/articles/{ID}")
+	id, err := strconv.Atoi(mux.Vars(req)["ID"])
+
+	if err != nil {
+		api.logger.Info("Troubles while parsing {ID} param: ", err)
+		msg := Message{
+			StatusCode: 400,
+			Message:    "Unapropriate ID value, don`t use ID as uncasting to int value.",
+			IsError:    true,
+		}
+		writer.WriteHeader(400)
+		json.NewEncoder(writer).Encode(msg)
+		return
+	}
+	article, ok, err := api.storage.Article().FindArticleByID(id)
+	if err != nil {
+		api.logger.Info("Troubles while accessing database table (articles) with ID, Error: ", err)
+		msg := Message{
+			StatusCode: 500,
+			Message:    "We have some troubles to accessing database. Try again later",
+			IsError:    true,
+		}
+		writer.WriteHeader(500)
+		json.NewEncoder(writer).Encode(msg)
+		return
+	}
+	if !ok {
+		api.logger.Info("Can not find article with that ID in database.")
+		msg := Message{
+			StatusCode: 404,
+			Message:    "Article with that ID does not exists in database.",
+			IsError:    true,
+		}
+		writer.WriteHeader(404)
+		json.NewEncoder(writer).Encode(msg)
+		return
+	}
+	writer.WriteHeader(200)
+	json.NewEncoder(writer).Encode(article)
+}
+
+func (api *API) DeleteArticleByID(writer http.ResponseWriter, req *http.Request) {
+	initHandlers(writer)
+	api.logger.Info("Delete Article by ID DELETE /api/v1/articles/{ID}")
+	id, err := strconv.Atoi(mux.Vars(req)["ID"])
+
+	if err != nil {
+		api.logger.Info("Troubles while parsing {ID} param: ", err)
+		msg := Message{
+			StatusCode: 400,
+			Message:    "Unapropriate ID value, don`t use ID as uncasting to int value.",
+			IsError:    true,
+		}
+		writer.WriteHeader(400)
+		json.NewEncoder(writer).Encode(msg)
+		return
+	}
+	_, ok, err := api.storage.Article().FindArticleByID(id)
+	if err != nil {
+		api.logger.Info("Troubles while accessing database table (articles) with ID, Error: ", err)
+		msg := Message{
+			StatusCode: 500,
+			Message:    "We have some troubles to accessing database. Try again later",
+			IsError:    true,
+		}
+		writer.WriteHeader(500)
+		json.NewEncoder(writer).Encode(msg)
+		return
+	}
+	if !ok {
+		api.logger.Info("Can not find article with that ID in database.")
+		msg := Message{
+			StatusCode: 404,
+			Message:    "Article with that ID does not exists in database.",
+			IsError:    true,
+		}
+		writer.WriteHeader(404)
+		json.NewEncoder(writer).Encode(msg)
+		return
+	}
+
+	_, err = api.storage.Article().DeleteByID(id)
+	if err != nil {
+		api.logger.Info("Troubles while deleting database element from table (articles) with ID, Error: ", err)
+		msg := Message{
+			StatusCode: 501,
+			Message:    "We have some troubles to accessing database. Try again later",
+			IsError:    true,
+		}
+		writer.WriteHeader(501)
+		json.NewEncoder(writer).Encode(msg)
+		return
+	}
+	writer.WriteHeader(202)
+	msg := Message{
+		StatusCode: 202,
+		Message:    fmt.Sprintf("Article with ID %d successfully deleted.", id),
+		IsError:    false,
+	}
+	json.NewEncoder(writer).Encode(msg)
+}
+
+func (api *API) PostUserRegister(writer http.ResponseWriter, req *http.Request) {
+	initHandlers(writer)
+	api.logger.Info("Post User Register POST /api/v1/user/register")
+	var user models.User
+	err := json.NewDecoder(req.Body).Decode(&user)
+	if err != nil {
+		api.logger.Info("Invalid json recieved from client")
+		msg := Message{
+			StatusCode: 400,
+			Message:    "Provided json is invalid",
+			IsError:    true,
+		}
+		writer.WriteHeader(400)
+		json.NewEncoder(writer).Encode(msg)
+		return
+	}
+	_, ok, err := api.storage.User().FindByLogin(user.Login)
+	if err != nil {
+		api.logger.Info("Troubles while accessing database table (users) with login, Error: ", err)
+		msg := Message{
+			StatusCode: 500,
+			Message:    "We have some troubles to accessing database. Try again later",
+			IsError:    true,
+		}
+		writer.WriteHeader(500)
+		json.NewEncoder(writer).Encode(msg)
+		return
+	}
+	if ok {
+		api.logger.Info("User with that Login already exists.")
+		msg := Message{
+			StatusCode: 400,
+			Message:    "User with that Login already exists in database.",
+			IsError:    true,
+		}
+		writer.WriteHeader(400)
+		json.NewEncoder(writer).Encode(msg)
+		return
+	}
+	userAdded, err := api.storage.User().Create(&user)
+	if err != nil {
+		api.logger.Info("Troubles while accessing database table (users), Error: ", err)
+		msg := Message{
+			StatusCode: 500,
+			Message:    "We have some troubles to accessing database. Try again later",
+			IsError:    true,
+		}
+		writer.WriteHeader(500)
+		json.NewEncoder(writer).Encode(msg)
+		return
+	}
+	msg := Message{
+		StatusCode: 201,
+		Message:    fmt.Sprintf("User {login:%s} successfully registered.", userAdded.Login),
+		IsError:    true,
+	}
+	writer.WriteHeader(201)
+	json.NewEncoder(writer).Encode(msg)
+}
+
+func (api *API) GetAllUsers(writer http.ResponseWriter, req *http.Request) {}
--- internal/app/api/helper.go
+++ internal/app/api/helper.go
@@ -1,10 +1,12 @@
 package api
 
 import (
-	"net/http"
-
 	"github.com/codeboris/specialist/storage"
 	"github.com/sirupsen/logrus"
+)
+
+var (
+	prefix string = "/api/v1"
 )
 
 func (a *API) configLoggerField() error {
@@ -17,9 +19,12 @@ func (a *API) configLoggerField() error {
 }
 
 func (a *API) configRouterField() {
-	a.router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello this is rest api!"))
-	})
+	a.router.HandleFunc(prefix+"/articles", a.GetAllArticles).Methods("GET")
+	a.router.HandleFunc(prefix+"/articles/{ID}", a.GetArticleByID).Methods("GET")
+	a.router.HandleFunc(prefix+"/articles/{ID}", a.DeleteArticleByID).Methods("DELETE")
+	a.router.HandleFunc(prefix+"/articles", a.PostArticle).Methods("POST")
+	a.router.HandleFunc(prefix+"/user/register", a.PostUserRegister).Methods("POST")
+	a.router.HandleFunc(prefix+"/users", a.GetAllUsers).Methods("GET")
 }
 
 func (a *API) configStorageField() error {
